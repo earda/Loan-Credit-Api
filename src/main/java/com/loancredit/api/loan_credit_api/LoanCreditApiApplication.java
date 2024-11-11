f469fa33107a72ed71ff5528cb372545b001fe9b
Erdem <earda230@gmail.com>
2024-11-11 23:32:27 +0300

    Initialize Project

---
+++ src/main/java/com/loancredit/api/loan_credit_api/LoanCreditApiApplication.java
@@ -0,0 +1,13 @@
+package com.loancredit.api.loan_credit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCreditApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCreditApiApplication.class, args);
+	}
+
+}
